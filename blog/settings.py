fd7d1373a3fc26c7d1cb3bf58e7c3f6229122aca
emericdefay <emericdefay@gmail.com>
2022-06-06 00:49:38 +0200

    psycopg2 in settings

--- blog/settings.py
+++ blog/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 if not DEBUG:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': config('DB_NAME'),
             'USER': config('DB_USER'),
             'PASSWORD': config('DB_PASSWORD'),
