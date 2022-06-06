cc1e91c25723b8343e7b486890268f0f35bdacef
emericdefay <emericdefay@gmail.com>
2022-06-06 07:03:18 +0200

    make another setting file instead

--- blog/settings.py
+++ blog/settings.py
@@ -93,13 +93,6 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # If you are using different databases for development and production.
 
 if not DEBUG:
-    if 'test' in sys.argv:
-        DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-            }
-        }
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
