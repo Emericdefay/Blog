b6ad81bca9e9fe2a7cc06a4d67cec6ad802ae5fb
emericdefay <emericdefay@gmail.com>
2022-06-06 15:54:39 +0200

    staticroot

--- blog/settings.py
+++ blog/settings.py
@@ -178,8 +178,8 @@ LOGOUT_REDIRECT_URL = '/'
 
 
 if not DEBUG:
-    STATIC_ROOT = 'static'
-    MEDIA_ROOT = 'media'
+    STATIC_ROOT = cast(str, os.path.join(BASE_DIR, 'static'))
+    MEDIA_ROOT = cast(str, os.path.join(BASE_DIR, 'media'))
 else:
     STATIC_ROOT = cast(str, os.path.join(BASE_DIR, 'static'))
     MEDIA_ROOT = cast(str, os.path.join(BASE_DIR, 'media'))
