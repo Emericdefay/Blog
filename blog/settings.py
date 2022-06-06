58fd12a7992bf32a6324bb562e1af787717fa3c5
emericdefay <emericdefay@gmail.com>
2022-06-06 23:57:41 +0200

    LOGGIN made to follow logs

--- blog/settings.py
+++ blog/settings.py
@@ -115,28 +115,29 @@ else:
         }
     }
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'logfile': {
-            'class': 'logging.handlers.WatchedFileHandler',
-            'filename': 'D:/home/site/wwwroot/myapp.log'
-        }
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['logfile'],
-            'level': 'ERROR',
-            'propagate': False
+if not DEBUG:
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'filters': {
+            'require_debug_false': {
+                '()': 'django.utils.log.RequireDebugFalse'
+            }
+        },
+        'handlers': {
+            'logfile': {
+                'class': 'logging.handlers.WatchedFileHandler',
+                'filename': 'D:/home/site/wwwroot/myapp.log'
+            }
+        },
+        'loggers': {
+            'django': {
+                'handlers': ['logfile'],
+                'level': 'ERROR',
+                'propagate': False
+            }
         }
     }
-}
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
