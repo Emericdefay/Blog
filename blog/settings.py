cf1b749f633ad9302b622ae8c41eb55a095bdde9
emericdefay <emericdefay@gmail.com>
2022-06-09 14:56:29 +0200

    npm build

--- blog/settings.py
+++ blog/settings.py
@@ -195,7 +195,7 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
-STATICFILES_STORAGE = ('whitenoise.storage.CompressedStaticFilesStorage') # CompressedManifestStaticFilesStorage
+STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage') # CompressedManifestStaticFilesStorage
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
