a033c2ad3bc78ef4a42dbd5a205814407e217e7d
emericdefay <emericdefay@gmail.com>
2022-11-25 18:52:44 +0100

    x

--- blog/settings.py
+++ blog/settings.py
@@ -229,13 +229,13 @@ LOGOUT_REDIRECT_URL = '/'
 if not DEBUG:
     #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-    # STATIC_ROOT = os.path.join(BASE_DIR, 'posts', 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'posts', 'static')
     MEDIA_ROOT = os.path.join(BASE_DIR, 'posts', 'media')
     #STATIC_ROOT = cast(str, os.getenv('STATIC_ROOT', os.path.join(BASE_DIR, 'staticfiles')))
     #MEDIA_ROOT = cast(str, os.getenv('MEDIA_ROOT', os.path.join(BASE_DIR, 'media')))
     pass
 else:
-    # STATIC_ROOT = os.path.join(BASE_DIR, 'posts', 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'posts', 'static')
     MEDIA_ROOT = os.path.join(BASE_DIR, 'posts', 'media')
 
 COMMENT_FLAGS_ALLOWED = 3
