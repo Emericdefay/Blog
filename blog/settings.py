8cbebf84fc28e533655d0850402715adc2fafdd3
emericdefay <emericdefay@gmail.com>
2022-11-25 18:26:19 +0100

    x

--- blog/settings.py
+++ blog/settings.py
@@ -227,10 +227,10 @@ LOGOUT_REDIRECT_URL = '/'
 
 
 if not DEBUG:
-    # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-    STATIC_ROOT = os.path.join(BASE_DIR, 'posts', 'static')
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'posts', 'media')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    #STATIC_ROOT = os.path.join(BASE_DIR, 'posts', 'static')
+    #MEDIA_ROOT = os.path.join(BASE_DIR, 'posts', 'media')
     #STATIC_ROOT = cast(str, os.getenv('STATIC_ROOT', os.path.join(BASE_DIR, 'staticfiles')))
     #MEDIA_ROOT = cast(str, os.getenv('MEDIA_ROOT', os.path.join(BASE_DIR, 'media')))
     pass
