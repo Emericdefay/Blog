cd8114ee211a2611a9a138b8686650c0b56ec9e2
emericdefay <emericdefay@gmail.com>
2022-11-25 17:03:59 +0100

    fix

--- blog/settings.py
+++ blog/settings.py
@@ -130,6 +130,13 @@ if DEBUG:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
